bee6f8b50e7d33a4ea8a78cda92e61d1f16383a1
estebanappform <75994156+estebanappform@users.noreply.github.com>
2020-12-14 19:04:21 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
-   connectedCallback() {
-      this.innerHTML = /* html */ 
-         `Copyright &copy; 2020 Guerra Mendoza Esteban.`;
-   }
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `Copyright &copy; 2020 Ricardo Armando Machorro Reyes.`;
+  }
 }
 customElements.define("mi-footer", MiFooter);
