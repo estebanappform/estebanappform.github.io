1747f41985a98fbcf16408df51e559a48ba843c4
estebanappform <75994156+estebanappform@users.noreply.github.com>
2020-12-14 19:02:21 -0600

    Update CtrlSolicitar.js

--- js/CtrlSolicitar.js
+++ js/CtrlSolicitar.js
@@ -1,10 +1,10 @@
-export class CtrlSolicitar {
-  /** @param {string} nombre */ 
-  calculaSolicitar(nombre) {
+export class CtrlSaludo {
+  /** @param {string} nombre */
+  calculaSaludo(nombre) {
     const nombreSinEspacios = nombre ? nombre.trim() : "";
     if (!nombreSinEspacios) {
-      throw new Error("Falta proporcinar el nombre.");
-    }  
-    return `saludo ${nombreSinEspacios}.`;
-  } 
+      throw new Error("Falta proporcionar el nombre.");
+    }
+    return `Hola ${nombreSinEspacios}.`;
+  }
 }
