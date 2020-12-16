ef6af827e1f60c26dd871a9b277e6d57b697da42
estebanappform <75994156+estebanappform@users.noreply.github.com>
2020-12-16 13:39:58 -0600

    Update CtrlSolicitar.js

--- js/CtrlSolicitar.js
+++ js/CtrlSolicitar.js
@@ -1,10 +1,32 @@
-export class CtrlSaludo {
-  /** @param {string} nombre */
-  calculaSaludo(nombre) {
-    const nombreSinEspacios = nombre ? nombre.trim() : "";
-    if (!nombreSinEspacios) {
-      throw new Error("Falta proporcionar el nombre.");
-    }
-    return `Hola ${nombreSinEspacios}.`;
-  }
+"use strict";
+
+var forma = document.getElementById("forma"),
+    salida = document.getElementById("salida");
+    
+forma.addEventListener("submit", procesa, false);
+forma["limpia"].addEventListener("click", limpiar, false);
+
+ 
+
+function procesa(){
+  var boleta = forma["boleta"].value,  
+      nombre = forma["nombre"].value, 
+      grupo = forma["grupo"].value, 
+      materia = forma["materia"].value,  
+      fecha = new Date(forma["fecha"].value),
+      miFecha;
+      miFecha = fecha.getDate() + 1 + "/"+ (fecha.getMonth() + 1) +"/"+fecha.getFullYear();
+    //salida.textContent="Boleta: "+boleta + " Nombre: "+ nombre+ " Grupo: "+ grupo+ " Teléfono: "+telefono + " Fecha: "+miFecha;
+    salida.innerHTML="<dfn>Boleta: "+boleta + "</dfn><br><dfn>Nombre: "+ nombre+ "</dfn><br><dfn>Grupo: "+ grupo+
+            "</dfn><br><dfn>Materia: "+materia + "</dfn><br><dfn>Fecha: "+miFecha+"</dfn>";
+}
+
+ 
+
+function limpiar(){
+    forma["clave"].value="";
+    forma["nombre"].value="";
+    forma["telefono"].value="";
+    forma["fecha"].value="";
+    salida.textContent="";
 }
